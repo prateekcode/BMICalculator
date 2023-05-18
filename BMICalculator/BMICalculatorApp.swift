f44c9df17bf8ad6b9ed5c99b527a1b9ece6ff7a1
prateekrai <prateek.rai@tiket.com>
2023-05-18 08:50:48 +0530

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Prateek Rai on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
